9c334b9aaf580b5d4f31b52348f38edf95d8eb3c
phuocchung123 <chungnvp197@gmail.com>
2024-05-15 20:08:03 +0700

    update

--- running_get_graph.py
+++ running_get_graph.py
@@ -10,76 +10,33 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-data=pd.read_csv('./data_chung/schneider50k.tsv',sep='\t',index_col=0)
+data=pd.read_csv('./data_chung/test.csv',index_col=0)
 
-# def new_smi_react(smi):
-#     rxnmapper = RXNMapper()
-#     try:
-#         mapped_smi=rxnmapper.get_attention_guided_atom_maps([smi])[0]['mapped_rxn']
-#         precusor,product=smi.split('>>')
-#         precusor1,product1=mapped_smi.split('>>')
 
-#         # Choose mapped precusor
-#         ele_react=precusor.split('.')
-#         ele_react1=precusor1.split('.')
-#         precusor_main=[i for i in ele_react if i not in ele_react1]
-#         precusor_str='.'.join(precusor_main)
-#         reagent=[i for i in ele_react if i in ele_react1]
-
-#         # Choose mapped product
-#         ele_pro=product.split('.')
-#         ele_pro1=product1.split('.')
-#         product_main=[i for i in ele_pro if i not in ele_pro1]
-#         product_main_2=[i for i in product_main if i not in precusor_main]
-#         product_str='.'.join(product_main_2)
-        
-#         new_react=precusor_str+'>>'+product_str
-#     except:
-#         new_react=np.nan
-
-#     return new_react
-
-# data['new_rxn']=data['rxn'].apply(new_smi_react)
-# data=data.dropna(subset=['new_rxn'])
+data_pretrain,data_test=train_test_split(data,size=0.2,stratify=data['label'].values)
+data_train,data_valid=train_test_split(data_pretrain,test_size=0.2,stratify=data_pretrain['label'].values)
 
 
 
-# Transfer from rxn_class to class
-with open('./data_chung/rxnclass2id.json','r') as f:
-    rxnclass2id=json.load(f)
-data['y']=[rxnclass2id[c] for c in data['rxn_class']]
-
-data_pretrain=data[data.split=='train']
-data_train,data_valid=train_test_split(data_pretrain,test_size=0.2,stratify=data_pretrain['y'].values)
-
-def to_categorical(y, num_classes):
-    """ 1-hot encodes a tensor """
-    return np.eye(num_classes, dtype='uint8')[y]
-
-
-rsmi_list=data['rxn'].values
+rsmi_list=data['rsmi'].values
 rmol_max_cnt = np.max([smi.split(">>")[0].count(".") + 1 for smi in rsmi_list])
 pmol_max_cnt = np.max([smi.split(">>")[1].count(".") + 1 for smi in rsmi_list])
 
 
 #get_data_train
-rsmi_list_train=data_train['rxn'].values
-y_list_train=data_train['y'].values
-y_list_train=to_categorical(y_list_train, 50)
-filename_train='./data_chung/data_train2.npz'
+rsmi_list_train=data_train['rsmi'].values
+y_list_train=data_train['label'].values
+filename_train='./data_chung/data_train_nonreagent.npz'
 get_graph_data(rsmi_list_train,y_list_train,filename_train,rmol_max_cnt,pmol_max_cnt)
 
 #get_data_valid
-rsmi_list_valid=data_valid['rxn'].values
-y_list_valid=data_valid['y'].values
-y_list_valid=to_categorical(y_list_valid, 50)
-filename_valid='./data_chung/data_valid2.npz'
+rsmi_list_valid=data_valid['rsmi'].values
+y_list_valid=data_valid['label'].values
+filename_valid='./data_chung/data_valid_nonreagent.npz'
 get_graph_data(rsmi_list_valid,y_list_valid,filename_valid,rmol_max_cnt,pmol_max_cnt)
 
 #get_data_test
-data_test=data[data['split']=='test']
-rsmi_list_test=data_test['rxn'].values
-y_list_test=data_test['y'].values
-y_list_test=to_categorical(y_list_test, 50)
-filename_test='./data_chung/data_test2.npz'
+rsmi_list_test=data_test['rsmi'].values
+y_list_test=data_test['label'].values
+filename_test='./data_chung/data_test_nonreagent.npz'
 get_graph_data(rsmi_list_test,y_list_test,filename_test,rmol_max_cnt,pmol_max_cnt)
