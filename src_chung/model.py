094640b3bcbade19f919e2d8d47cbe328cc926c5
phuocchung123 <chungnvp197@gmail.com>
2024-06-14 12:01:02 +0700

    update

--- src_chung/model.py
+++ src_chung/model.py
@@ -117,7 +117,7 @@ class reactionMPNN(nn.Module):
         node_in_feats,
         edge_in_feats,
         pretrained_model_path=None,
-        readout_feats=300,
+        readout_feats=1024,
         predict_hidden_feats=512,
         prob_dropout=0.1,
         # cuda=torch.device(f"cuda:{torch.cuda.current_device()}")
